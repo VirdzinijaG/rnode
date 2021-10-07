3bc522470c6668f963a3c7cf4d335963ad654ca4
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-10-07 17:41:11 +0300

    10.07

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ const mysql = require('mysql')
 const cors = require('cors')
 app.use(cors())
 
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json());
+
 const con = mysql.createConnection({
     host: 'localhost',
-    user: 'nodejs',
-    password: 'nodejs123456',
+    user: 'root',
+    password: '',
     database: 'articles'
 })
 
@@ -18,6 +23,8 @@ con.connect(err => {
     }
     console.log('Yes!');
 })
+
+
 
 
 
@@ -33,16 +40,63 @@ con.connect(err => {
 //     res.send(`Pats tu ${req.params.id}`);
 // })
 
-
-// iraso nauja posta
-app.get('/posts', (req, res) => {
-    res.send(`Pats tu ${req.params.id}`)
+// Iraso nauja posta
+// INSERT INTO table_name (column1, column2, column3,...)
+// VALUES (value1, value2, value3,...)
+app.post('/posts', (req, res) => {
+    console.log(req.body.title)
+    const sql = `
+        INSERT INTO posts
+        (title, body)
+        VALUES (?, ?)
+        `;
+    con.query(sql, [req.body.title, req.body.body], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send(result);
+    })
 })
+
+// Trina posta
+// DELETE FROM table_name
+// WHERE some_column = some_value
+app.delete('/posts/:id', (req, res) => {
+    const sql = `
+        DELETE FROM posts
+        WHERE id = ?
+        `;
+    con.query(sql, [req.params.id], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send(result);
+    })
+})
+
+//Redagavimas
+// UPDATE table_name
+// SET column1=value, column2=value2,...
+// WHERE some_column=some_value 
+app.put('/posts/:id', (req, res) => {
+    const sql = `
+        UPDATE posts
+        SET title = ?, body = ?
+        WHERE id = ?
+        `;
+    con.query(sql, [req.body.title, req.body.body, req.params.id], (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.send(result);
+    })
+})
+
 
 
 // rodo visus postus
 app.get('/posts', (req, res) => {
-    con.query('SELECT * FROM posts', (err, results) => {
+    con.query('SELECT * FROM posts ORDER BY id DESC', (err, results) => {
         if (err) {
             throw err;
         }
